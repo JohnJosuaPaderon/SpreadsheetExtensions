fa43a15a100098cc01adcf614c8ecd1f0d6c2bcb
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-03-19 21:41:28 +0800

    Samples

---
+++ samples/SpreadsheetExtensions.EPPlus.SampleConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using OfficeOpenXml;
+using SpreadsheetExtensions.Styles;
+using System;
+using System.IO;
+
+namespace SpreadsheetExtensions.EPPlus.SampleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            Console.WriteLine("SpreadsheetExtensions.EPPlus.SampleConsoleApp");
+
+            var file = Path.Combine(Directory.GetCurrentDirectory(), "sample_epplus.xlsx");
+
+            if (!File.Exists(file))
+            {
+                Console.WriteLine($"The file '{file}' does not exists");
+                return;
+            }
+
+            using var package = new ExcelPackage(new FileInfo(file));
+            using var workbook = package.Workbook;
+            using var worksheet = workbook.Worksheets[0];
+
+            worksheet.Write(1, 1, 5, 5, "SAMPLE TEXT")
+                .Merge(true)
+                .SetFontBold(true)
+                .SetFontItalic(true)
+                .SetFontUnderline(true)
+                .SetBorderBottom(new SpreadsheetBorder
+                {
+                    Style = SpreadsheetBorderStyle.Thick,
+                    Color = new SpreadsheetColor(255, 0, 0, 255)
+                })
+                .SetBorderRight(new SpreadsheetBorder
+                {
+                    Style = SpreadsheetBorderStyle.Dotted,
+                    Color = SpreadsheetColor.FromDotNetColor(System.Drawing.Color.Green)
+                })
+                .SetVerticalAlignment(SpreadsheetVerticalAlignment.Center)
+                .SetHorizontalAlignment(SpreadsheetHorizontalAlignment.Center);
+            package.Save();
+        }
+    }
+}
---
+++ samples/SpreadsheetExtensions.GemBox.Spreadsheet.SampleConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using GemBox.Spreadsheet;
+using SpreadsheetExtensions.Styles;
+using System;
+using System.IO;
+
+namespace SpreadsheetExtensions.GemBox.Spreadsheet.SampleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+            Console.WriteLine("SpreadsheetExtensions.GemBox.Spreadsheet.SampleConsoleApp");
+
+            var file = Path.Combine(Directory.GetCurrentDirectory(), "sample_gembox_spreadsheet.xlsx");
+
+            if (!File.Exists(file))
+            {
+                Console.WriteLine($"The file '{file}' does not exists");
+                return;
+            }
+
+            var excelFile = ExcelFile.Load(file);
+            var worksheet = excelFile.Worksheets[0];
+
+            worksheet.Write(0, 0, 4, 4, "SAMPLE TEXT")
+                .Merge(true)
+                .SetFontBold(true)
+                .SetFontItalic(true)
+                .SetFontUnderline(true)
+                .SetBorderBottom(new SpreadsheetBorder
+                {
+                    Style = SpreadsheetBorderStyle.Thick,
+                    Color = new Styles.SpreadsheetColor(255, 0, 0, 255)
+                })
+                .SetBorderRight(new SpreadsheetBorder
+                {
+                    Style = SpreadsheetBorderStyle.Dotted,
+                    Color = Styles.SpreadsheetColor.FromDotNetColor(System.Drawing.Color.Green)
+                })
+                .SetVerticalAlignment(SpreadsheetVerticalAlignment.Center)
+                .SetHorizontalAlignment(SpreadsheetHorizontalAlignment.Center);
+            excelFile.Save(file);
+        }
+    }
+}
